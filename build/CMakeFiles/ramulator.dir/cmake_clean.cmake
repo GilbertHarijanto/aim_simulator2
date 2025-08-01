6054348149ccca22c2f4417cb636fa6ec2fc8723
GilbertHarijanto <lgharijanto123@gmail.com>
2025-08-01 13:30:01 -0700

    add

--- build/CMakeFiles/ramulator.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/Users/gilbertharijanto/Developer/skhynix/aim_simulator/libramulator.dylib"
-  "/Users/gilbertharijanto/Developer/skhynix/aim_simulator/libramulator.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/ramulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/_deps/yaml-cpp-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gilbertharijanto/Developer/skhynix/aim_simulator/ext/yaml-cpp
-# Build directory: /Users/gilbertharijanto/Developer/skhynix/aim_simulator/build/_deps/yaml-cpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
